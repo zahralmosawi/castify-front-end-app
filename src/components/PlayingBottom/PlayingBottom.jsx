8865e0c4186d79b7939d5be039ed0e75ae2afe7e
zahralmosawi <zalmosawii65@gmail.com>
2025-08-19 19:30:48 +0300

    adding new component - bottom

---
+++ src/components/PlayingBottom/PlayingBottom.jsx
@@ -0,0 +1,28 @@
+import PodcastPlayingContext, { useAudio } from '../../Contexts/PodcastPlayingContext'
+import { useContext } from 'react'
+
+const PlayingBottom = () => {
+    const { currentPodcast, isPlaying, playPodcast, pausePodcast } = useContext(PodcastPlayingContext)
+
+    if (!currentPodcast) {
+        return null 
+    }
+
+    const handlePlayPause = () => {
+        if (isPlaying) {
+            pausePodcast()
+        } else {
+            playPodcast(currentPodcast)
+        }
+    }
+
+    return (
+        <div>
+            <h3>Now Playing</h3>
+            <p><strong>{currentPodcast.title} by {currentPodcast.crator}</strong></p>
+            <button onClick={handlePlayPause}>{isPlaying ? 'Pause' : 'Play'}</button>
+        </div>
+    )
+}
+
+export default PlayingBottom
