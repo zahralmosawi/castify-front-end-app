672d3957cc46d7dc86d4ee511b93648e62e3a306
zahralmosawi <zalmosawii65@gmail.com>
2025-08-19 13:09:09 +0300

    added change password component in the app

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import EditProfile from './components/EditProfile/EditProfile';
 import PodcastDetails from './components/PodcastDetails/PodcastDetails';
 import CreateBoard from './components/Boards/CreateBoard';
 import BoardDetails from './components/BoardDetails/BoardDetails.jsx';
+import ChangePasswordForm from './components/ChangePasswordForm/ChangePasswordForm.jsx'
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem('token'));
@@ -47,6 +48,7 @@ function App() {
           <Route path="/profile/edit" element={<EditProfile />} />
           <Route path="/boards/new" element={<ProtectedRoute><CreateBoard /></ProtectedRoute>} />
           <Route path="/boards/:id" element={<ProtectedRoute><BoardDetails /></ProtectedRoute>} />
+          <Route path="/changePassword" element={<ProtectedRoute><ChangePasswordForm /></ProtectedRoute>} />
         </Routes>
       </div>
     </Router>
