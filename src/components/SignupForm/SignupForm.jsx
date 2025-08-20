0625c893d98361d31542175cd79a586bc171be28
zahralmosawi <zalmosawii65@gmail.com>
2025-08-20 17:33:30 +0300

    adding signup page style

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import './SignupForm.css';
 
 function SignupForm() {
     const [formData, setFormData] = useState({
@@ -39,15 +40,35 @@ function SignupForm() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <div className="signup-container">
+        <form onSubmit={handleSubmit} className="signup-form">
             <h2>Sign Up</h2>
+
+            <div className='form-group'>
             <input name="username" placeholder="Username" onChange={handleChange} required />
+            </div>
+
+            <div className='form-group'>
             <input name="email" type="email" placeholder="Email" onChange={handleChange} required />
+            </div>
+            
+            <div className='form-group'>
             <input name="password" type="password" placeholder="Password" onChange={handleChange} required />
+            </div>
+
+            <div className='form-group'>
             <input name="name" placeholder="Name" onChange={handleChange} />
+            </div>
+
+            <div className='form-group'>
             <textarea name="bio" placeholder="Bio" onChange={handleChange} />
+            </div>
+
+            <div className='singup-button'>
             <button type="submit">Sign Up</button>
+            </div>
         </form>
+        </div>
     )
 }
 
