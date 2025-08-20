8bbd12053b3779e4c17b5492ddc752b7ab90abbe
zahralmosawi <zalmosawii65@gmail.com>
2025-08-20 21:30:38 +0300

    updating login and signup form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 import './LoginForm.css';
 
@@ -28,29 +28,28 @@ function LoginForm({ onLogin }) {
 
     return (
         <div className="login-constainer">
-        <form onSubmit={handleSubmit} className="login-form">
-            <h2>Welcome Back</h2>
-
-            <div className="form-group">
-            <input 
-                placeholder="Username"
-                value={username}
-                className="login-input"
-                onChange={event => setUsername(event.target.value)}
-            />
-            </div>
-            <div className="form-group">
-            <input 
-                placeholder="Password"
-                type="password"
-                value={password}
-                className="login-input"
-                onChange={event => setPassword(event.target.value)}
-            />
-            </div>
-
-            <button type='submit' className="login-button">Login</button>
-        </form>
+            <form onSubmit={handleSubmit} className="login-form">
+                <h2>Welcome Back</h2>
+                <div className="form-group">
+                    <input 
+                        placeholder="Username"
+                        value={username}
+                        className="login-input"
+                        onChange={event => setUsername(event.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                        placeholder="Password"
+                        type="password"
+                        value={password}
+                        className="login-input"
+                        onChange={event => setPassword(event.target.value)}
+                    />
+                </div>
+                <button type='submit' className="login-button">Login</button>
+                <Link to="/signup" style={{textAlign: 'center', display: 'block', marginTop: '1.5rem'}}>Don't have an account? Sign up</Link>
+            </form>
         </div>
     )
 }
